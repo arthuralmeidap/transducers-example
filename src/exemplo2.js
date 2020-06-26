e49ed3f9f8f3abcc3bf6fbad657a423229208bb7
tukka <arthur.almeidapereira@gmail.com>
2020-06-26 17:00:29 -0300

    exemplo 2 transformado em reduce

---
+++ src/exemplo2.js
@@ -0,0 +1,41 @@
+const { double, isBiggerThanTen, sum, addOne } = require("./operations");
+
+const list = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+];
+
+const doubleReducer = (acc, value) => acc.concat(double(value));
+const addOneReducer = (acc, value) => acc.concat(addOne(value));
+
+const isBiggerThanTenReducer = (acc, value) =>
+  isBiggerThanTen(value) ? acc.concat(value) : acc;
+
+console.log(`double using map: ${list.map(double)}`);
+console.log(`double using reduce: ${list.reduce(doubleReducer, [])}`);
+console.log("\n\n");
+console.log(`addOne using map: ${list.map(addOne)}`);
+console.log(`addOne using reduce: ${list.reduce(addOneReducer, [])}`);
+console.log("\n\n");
+console.log(`isBiggerThanTen using filter: ${list.filter(isBiggerThanTen)}`);
+console.log(
+  `isBiggerThanTen using reduce: ${list.reduce(isBiggerThanTenReducer, [])}`
+);
