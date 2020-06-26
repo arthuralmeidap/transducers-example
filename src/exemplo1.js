a4c685599ffd104bdbc8a9cb57fd3f4959e192db
tukka <arthur.almeidapereira@gmail.com>
2020-06-26 16:41:20 -0300

    refac: movido funções para novo arquivo

--- src/exemplo1.js
+++ src/exemplo1.js
@@ -1,10 +1,7 @@
 const R = require("ramda");
+const { double, addOne, isBiggerThanTen, sum } = require("./operations");
 const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const double = (n) => n * 2;
-const addOne = (n) => n + 1;
-const isBiggerThanTen = (n) => n > 10;
-const sum = (acc, value) => acc + value;
 const sumList = list
   .map(addOne)
   .map(double)
---
+++ src/operations.js
@@ -0,0 +1,6 @@
+module.exports = {
+  double: (n) => n * 2,
+  addOne: (n) => n + 1,
+  isBiggerThanTen: (n) => n > 10,
+  sum: (acc, value) => acc + value,
+};
