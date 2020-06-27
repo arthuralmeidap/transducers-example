a808e7d9ed0ae88ba80672031b882b368360f449
tukka <arthur.almeidapereira@gmail.com>
2020-06-26 21:20:40 -0300

    exemplo 3 com composição intermediárias

--- src/exemplo3.js
+++ src/exemplo3.js
@@ -6,6 +6,7 @@ const {
   mapReducer,
   filterReducer,
 } = require("./operations");
+const { compose } = require("ramda");
 
 const doubleIntermediateReducer = mapReducer(double);
 const addOneIntermediateReducer = mapReducer(addOne);
@@ -15,3 +16,11 @@ const isBiggerIntermediateReducer = filterReducer(isBiggerThanTen);
 const doubleReducer = doubleIntermediateReducer(combinator);
 const addOneReducer = addOneIntermediateReducer(combinator);
 const isBiggerReducer = isBiggerIntermediateReducer(combinator);
+
+const addOneAndDoubleReducer = compose(
+  addOneIntermediateReducer,
+  doubleIntermediateReducer,
+  isBiggerIntermediateReducer
+)(combinator);
+
+console.log([1, 2, 3, 4, 5, 6].reduce(addOneAndDoubleReducer, []));
